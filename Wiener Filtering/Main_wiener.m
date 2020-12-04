a8158f04267c8b5ce6ba43920d0e63ab196de0c2
aomanhao <1014325364@qq.com>
2020-12-04 23:41:05 +0800

    add wiener

--- Wiener Filtering/Main_wiener.m
+++ Wiener Filtering/Main_wiener.m
@@ -14,67 +14,30 @@ if size(img,3) == 3
 else
 end
 
-J=imnoise(I,'gaussian',0,0.01);
- 
-Mywiener2 = wiener2(J,[3 3]);
- 
+I_noise=imnoise(I,'gaussian',0,0.01);
+figure;imshow(I);title('灰度图');
+figure;imshow(I_noise);title('加噪图');
+% 维纳滤波
+Out_wiener = wiener2(I_noise,[3 3]);
+% 均值滤波
 Mean_temp = ones(3,3)/9;
-Mymean = imfilter(J,Mean_temp);
+Out_mean = imfilter(I_noise,Mean_temp);
  
-figure(1);
-subplot(121),imshow(Mywiener2),title('维纳滤波器输出');
-subplot(122),imshow(uint8(Mymean),[]),title('均值滤波器的输出');
+figure;
+imshow(Out_wiener);title('维纳滤波输出');
+figure;
+imshow(uint8(Out_mean),[]);title('均值滤波输出');
 
-%***********************维娜复原程序********************
-figure(2);
-subplot(231),imshow(I),title('原始图像');
- 
-LEN = 20;
-THETA =10;
-PSF = fspecial('motion',LEN,THETA);
- 
-Blurred = imfilter(I,PSF,'circular');
-subplot(232),imshow(Blurred),title('生成的运动的模糊的图像');
- 
-noise = 0.1*randn(size(I));
-subplot(233),imshow(im2uint8(noise)),title('随机噪声');
- 
-BlurredNoisy=imadd(Blurred,im2uint8(noise));
-subplot(234),imshow(BlurredNoisy),title('添加了噪声的模糊图像');
- 
-Move=deconvwnr(Blurred,PSF);
-subplot(235),imshow(Move),title('还原运动模糊的图像');
 
-nsr = sum(noise(:).^2)/sum(im2double(I(:)).^2);
-wnr2 = deconvwnr(BlurredNoisy,PSF,nsr);
-subplot(236),imshow(wnr2),title('还原添加了噪声的图像');
-
-%*************************维纳滤波应用于边缘提取****************************
-N = wiener2(I,[3,3]);%选用不同的维纳窗在此修改
-M = I - N;
-My_Wedge = im2bw (M,5/256);%化二值图像
-BW1 = edge(I,'prewitt');
-BW2 = edge(I,'canny');
-BW3 = edge(I,'zerocross');
-BW4 = edge(I,'roberts');
- 
-figure(3)
-subplot(2,4,[3 4 7 8]),imshow(My_Wedge),title('应用维纳滤波进行边沿提取');
-subplot(241),imshow(BW1),title('prewitt');
-subplot(242),imshow(BW2),title('canny');
-subplot(245),imshow(BW3),title('zerocross');
-subplot(246),imshow(BW4),title('roberts');
-
-%*************************维纳滤波应用于图像增强***************************
+%% *************************维纳滤波应用于图像增强***************************
 for i = [1 2 3 4 5]
     K = wiener2(I,[5,5]);
 end
  
-  K = K + I;
+K = K + I;
  
-figure(4);
-subplot(121),imshow(I),title('原始图像');
-subplot(122),imshow(K),title('增强后的图像');
+figure;imshow(I),title('原始图像');
+figure;imshow(K),title('增强后的图像');
 
 
 
