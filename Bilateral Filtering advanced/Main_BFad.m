701907952b60811c5c04f7b260b0068128c24eb4
aomanhao <1014325364@qq.com>
2024-12-29 21:03:57 +0800

    add joint BF

--- Bilateral Filtering advanced/Main_BFad.m
+++ Bilateral Filtering advanced/Main_BFad.m
@@ -1,4 +1,4 @@
-%% 程序分享 
+%% 程序分享
 % 个人博客 www.aomanhao.top
 % Github https://github.com/AomanHao
 % CSDN https://blog.csdn.net/Aoman_Hao
@@ -6,28 +6,42 @@
 clear
 close all
 clc
-%% %%%%%%%%%%%%%%%图像%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% %%%%%%%%%%%%%%% load data %%%%%%%%%%%%%%%%%
+addpath('./methods/');
 I=imread('3096.jpg');
 
 if size(I,3) == 3
-   I=rgb2gray(I);
+    I=rgb2gray(I);
 else
 end
 I=im2double(I);
-figure;imshow(I);title('(a)原始图像');imwrite(I,'1.jpg');
+figure;imshow(I);title('(a)原始图像');
+
+
 % I=I;%不加噪声
 % I=imnoise(I,'speckle',deta_2);
 % I=imnoise(I,'salt & pepper',0.05); %加噪图
-img1=imnoise(I,'gaussian',0,0.01); % 加高斯噪声
+I=imnoise(I,'gaussian',0,0.001); % 加高斯噪声
+imwrite(I,strcat('./result/','re','.png'));
 
 %% 设置参数
-w     = 5;       % bilateral filter half-width
-sigma = [3 0.1]; % bilateral filter standard deviations
+w     = 1;       % bilateral filter half-width
 
-%% 双边滤波
-bflt_img1 = jbfilter2(img1,img1,w,sigma);
-
-%% 输出图像
-figure;imshow(I); title('Input Image');
-figure;imshow(img1); title('Noise Image');
-figure;imshow(bflt_img1);title('Result of Bilateral Filtering');
+tic;
+method = 'bfilter2';
+switch method
+    case 'bfilter2'
+        %% 双边滤波
+        sigma = [2 0.1]; % bilateral filter standard deviations
+        result = bfilter2(I,w,sigma);
+        
+    case 'Joint_bfilter2'
+        %% 联合双边滤波
+        sigma = [2, 0.1, 0.5, 0.5]; 
+        conf.BF_type = 'BiF';%BiF  SiF
+        result = fun_Joint_bfilter2(I,w,sigma);
+        
+end
+toc;
+t=toc;
+imwrite(double(result),strcat('./result/',method,'_out','.png'));
